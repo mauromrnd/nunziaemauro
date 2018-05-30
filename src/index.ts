31c972b232310aca98eef71304f74669ce0f1ce5
Mauro Miranda <grande.emme@gmail.com>
2018-05-31 00:25:06 +0200

    Nuova struttura del progetto
    Sostituito gatsby con webpack e typescript

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+
+import 'normalize.css/normalize.css';
+
+import 'bootstrap/scss/bootstrap.scss';
+import './styles/index.scss';
+
+import 'jquery/dist/jquery.js';
+import 'bootstrap/js/src/collapse.js';
+
+//import './ts/map.ts'
+import './ts/foto.ts'
---
+++ src/ts/foto.ts
@@ -0,0 +1,10 @@
+var i, e, d = document, s = "script";
+
+i = d.createElement("script");
+
+i.async = 1;
+i.src = "https://cdn.curator.io/published/d5e0f6ef-254d-4d01-8491-ee25d25fcda5.js";
+
+e = d.getElementsByTagName(s)[0]; 
+
+e.parentNode.insertBefore(i, e);
